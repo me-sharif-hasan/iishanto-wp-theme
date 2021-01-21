c52a0a478f8993d7a9ce7226303676e7613efb8d
Sharif Hasan <me.sharif.hasan@gmail.com>
2021-01-21 21:02:38 +0600

    Kind of ....

---
+++ 404.php
@@ -0,0 +1,18 @@
+<?php get_header() ?>
+<section class="blog-area section">
+	<div class="row">
+		<div class="col-lg-8 col-md-12">
+			<h1>
+				Ops. The page you requested is not found.
+			</h1>
+		</div>
+		<div class="col-lg-4 col-md-12 no-left-padding">
+			<div class="single-post info-area">
+				<?php get_sidebar() ?>
+			</div>
+		</div>
+	</div>
+</section>
+<?php get_footer() ?>
+</body>
+</html>
